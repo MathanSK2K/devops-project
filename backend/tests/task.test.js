cf48fb9e1ad4bf61240b812d65ec706c91e23082
MATHAN KUMAR SEKAR <mathan-kumar.sekar@rntbci-nissan.com>
2025-09-28 11:09:38 +0530

    initial commit

---
+++ backend/tests/task.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const {app,server} = require('../index');
+const mongoose = require('mongoose');
+
+describe('GET api/tasks',()=>{
+    it('should return 200 OK',async()=>{
+        const res = await request(app).get('/api/tasks');
+        expect(res.statusCode).toEqual(200);
+    });
+    it('should return Array',async()=>{
+        const res = await request(app).get('/api/tasks');
+        expect(Array.isArray(res.body)).toBe(true);
+    });
+});
+
+afterAll(async()=>{
+    await mongoose.connection.close();
+    await server.close();
+})
